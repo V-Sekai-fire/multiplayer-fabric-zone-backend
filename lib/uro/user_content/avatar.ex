f6a02e2614dba41b9b6a347ef74909441a1ec037
Ben Botwin <ben.botwin@gmail.com>
2025-04-15 14:09:52 -0400

    ran mix format

--- lib/uro/user_content/avatar.ex
+++ lib/uro/user_content/avatar.ex
@@ -2,8 +2,8 @@ defmodule Uro.UserContent.Avatar do
   import Ecto.Changeset
 
   use Uro.UserContent.UserContent,
-      join_table_name: "backpack_avatars",
-      schema_atom: :avatar
+    join_table_name: "backpack_avatars",
+    schema_atom: :avatar
 
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
